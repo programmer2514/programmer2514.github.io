581b024e517058a7e2a62d118ad7cbb902688e52
TenorTheHusky <programmer2514@gmail.com>
2023-10-29 17:30:57 -0400

    Add LinkedIn links

--- include/footer/footer.js
+++ include/footer/footer.js
@@ -4,15 +4,17 @@ class Footer {
         this.element = document.getElementsByTagName("footer")[0];
         this.element.innerHTML = `
             <div id="footer-top">
-                <span>Copyright &copy; 2023 Benjamin Pryor</span>
+                <span></span>
                 <nav id="footer-links">
-                    <a target="_blank" href="https://github.com/programmer2514"><img src="/img/svg/github.svg" /></a>
                     <a target="_blank" href="https://mailhide.io/e/kHCbTHeA"><img src="/img/svg/email.svg" /></a>
+                    <a target="_blank" href="https://github.com/programmer2514"><img src="/img/svg/github.svg" /></a>
+                    <a target="_blank" href="https://www.linkedin.com/in/benjamin-pryor-1a7099295/"><img src="/img/svg/linkedin.svg" /></a>
                     <a target="_blank" href="https://discord.com/users/563652755814875146/"><img src="/img/svg/discord.svg" /></a>
                     <a target="_blank" href="https://kbin.social/u/TenorTheHusky"><img src="/img/svg/kbin.svg" /></a>
                 </nav>
             </div>
         `;
+        this.element.querySelector('span').innerHTML = `Copyright &copy; ${new Date().getFullYear()} Benjamin Pryor`;
         window.addEventListener('resize', function() {
             try {
                 footer.reposition();
