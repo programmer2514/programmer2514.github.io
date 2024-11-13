eb1f01e7ea51c070a6c3c10356aabde1d1834e27
Benjamin Pryor <programmer2514@gmail.com>
2024-11-13 17:17:30 -0500

    Update redirects

--- include/redirect.js
+++ include/redirect.js
@@ -1,9 +1,7 @@
-let redirectString = gup('l');
+let redirectString = window.location.href.match(/[?#](.*)/)[1];
 
-if (redirectString === 'cui-changelog')
+if (redirectString === 'cui-changelog' || redirectString === 'l=cui-changelog')
   window.location.replace('https://github.com/programmer2514/BetterDiscord-CollapsibleUI/blob/main/CHANGELOG.md');
 
-function gup(name) {
-  name = RegExp('[?&]' + name.replace(/([[\]])/, '\\$1') + '=([^&#]*)');
-  return (window.location.href.match(name) || ['', ''])[1];
-}
+if (redirectString === 'disability')
+  window.location.replace('https://programmer2514.github.io/MiamiOH-AAA211-Final-Project/public/');
